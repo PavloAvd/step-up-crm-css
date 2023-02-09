b1c56efe7e16ad8c373118fcebd6b577e549a97e
PavloAvd <pavlo.avd@gmail.com>
2023-02-10 01:39:59 +0200

    modal window emit prop

---
+++ src/components/AppModal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div :class="classModal">
+            <span @click="$emit('close')">Закрити</span>
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+const emits= defineEmits(['close'])
+const props = defineProps({
+    classModal: String
+})
+
+   
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,51 +1,81 @@
 <template>
-  <div class="homeContainer">
-    <div class="header">
-      <h2 class="">Вітаємо на освітньому танцювальному порталі колективу</h2>
-      <h1 class="">STEP UP SHTURM</h1>
+  <header>
+    <div class="container">
+      <div class="homeTitle">
+        <h2>Вітаємо на освітньому танцювальному порталі колективу</h2>
+        <h1>STEP UP SHTURM</h1>
+      </div>
     </div>
-    <div>
+  </header>
+  <section>
+    <div class="container">
       <h2>Фото з тренувань</h2>
+        <div class="photoPart">
+          <div  class="sliderListMobile">
+            <div>
+              <img 
+                v-for="(item, index) in homePagePhotos"
+                :key="index"
+                :src="require(`@/assets/${item}`)"
+                class="imgHome" 
+                alt=""
+              >
+            </div>
+          </div>
+        </div>
+    </div>
+  </section>
+  <section>
+    <div class="container">
+      <h2>Відео</h2> 
+      <div class="sliderListMobile">  
+        <iframe
+          v-for="item in store.getters['gallery/getHomePageVideo']" 
+          class="homePageVideo"    
+          :src="item"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen  
+        ></iframe>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container">
       <div class="sliderListMobile" >
-        <div v-for="item in homePagePhotos">
-          <img :src="require(`@/assets/${item}`)" class="imgHome" alt="">
-        </div>
-      </div>
-    </div>
-
-    <div class="pagePart">
-      <div class="sliderListMobile">
-        <div v-for="item in store.getters['gallery/getHomePageVideo']"  >
-          <iframe
-              class="homePageVideo"
-            :src="item"
-            title="YouTube video player"
-            frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-          ></iframe>
-        </div>
-      </div>
-    </div>
-    <div class="sliderListMobile" >
-      <div class="card">
-        <span>{{ store.getters['feedbackList']  }}</span>
-      </div>
-      <div  v-for="(item, idx) in feedbacks" :key="idx">
         <div class="card">
-          <span>{{ item.name }}</span>
-          <span>{{ item.comment }}</span>
+          <span>{{ store.getters['feedbackList']  }}</span>
+        </div>
+        <div  v-for="(item, idx) in feedbacks" :key="idx">
+          <div class="card">
+            <span>{{ item.name }}</span>
+            <span>{{ item.comment }}</span>
+          </div>
         </div>
       </div>
+      <AppButton title="Залишити відгук" @click="openModal = true"/>
+      <app-modal v-if="openModal" classModal="home-modal" @close="openModal = false">
+        <div class="">
+          <div class="feedback">
+            <label for="feedback">Залишити відгук</label>
+            <input type="text" id="feedback" class="input"/>
+            <AppButton title="Відправити" @click="store.dispatch('gallery/loadHomePageVideo')"></AppButton>
+          </div>
+        </div>
+      </app-modal>
     </div>
-
-    <div class="pagePart feedback">
-      <label for="feedback">Залишити відгук</label>
-      <input type="text" id="feedback" class="input"/>
-      <AppButton title="Відправити" @click="store.dispatch('gallery/loadHomePageVideo')"></AppButton>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="social-links">
+        <p>Instagram</p>
+        <p>Facebook</p>
+        <p>YouTube</p>
+      </div>
     </div>
-<!--    <carousel class=""/>-->
-  </div>
+  </footer>
+<!--    <carousel class=""/>--> 
 </template>
 
 <script setup>
@@ -53,7 +83,9 @@ import AppButton from '@/components/ui/button/AppButton.vue';
 import Carousel from '@/components/Carousel'
 import { useStore } from 'vuex';
 import { onMounted, ref, computed } from 'vue';
+import AppModal from '@/components/AppModal.vue';
 
+let openModal = ref(false)
 const feedbacks = [
   { name: 'Павло: ', comment : 'Найкраща хореографія тільки тут' },
   { name: 'Павло: ', comment : 'Найкраща хореографія тільки тут' },
@@ -74,14 +106,15 @@ let imgLink = computed(img => {
   return require('@/assets/' + img)
 })
 
+
 const store = useStore()
-// let imgLink = computed( ()=> {
-//   return store.getters["gallery/hpiLink"]
-// })
+
 onMounted( async () => {
     await store.dispatch('gallery/loadHomePageVideo')
     await store.dispatch('message/loadFeedback')
 })
+
+
 // onMounted( async () => {
 //   await store.dispatch('gallery/loadCarousel')
 // })
